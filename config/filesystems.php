ab414b559df62a93f271b44d06a6b5b580951dbe
arizalseptino <arizalseptino12@gmail.com>
2026-04-07 14:42:51 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL') ?? '', '/').'/storage',
+            'url' => env('APP_URL', 'https://greenhouse-laravel.vercel.app') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
